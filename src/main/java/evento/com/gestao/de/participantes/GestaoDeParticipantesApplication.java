96b4aad821621e1c1913facf4d79e571b33abd6e
Entony Jovino <entonysantos@outlook.com.br>
2025-05-06 15:27:54 -0300

    feat: criação do projeto

---
+++ src/main/java/evento/com/gestao/de/participantes/GestaoDeParticipantesApplication.java
@@ -0,0 +1,13 @@
+package evento.com.gestao.de.participantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeParticipantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeParticipantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evento/com/gestao/de/participantes/GestaoDeParticipantesApplicationTests.java
@@ -0,0 +1,13 @@
+package evento.com.gestao.de.participantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeParticipantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
